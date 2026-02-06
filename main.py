fc05c919399c5cadcae23a83807b289581134d30
Hor1e <romandementev929@gmail.com>
2026-02-06 20:34:13 +0500

    I FIXED IT YOOOOOOOO

--- main.py
+++ main.py
@@ -19,30 +19,32 @@ try:
     list = []
     listprices = []
     listlinks = []
-        
+    listoflists = []
     #Заполнение списков с названиями игр и их цены(Со скидками).
     for sells in soup.find_all("span", class_="title", limit=25):
         list.append(sells.get_text())
 
-    #for sells in soup.find_all("div",class_="discount_final_price", limit=25):
-        #listprices.append(sells.get_text())
-    
-    #Собрал ссылки, пока без вывода
-    for sellss in soup.find_all("div",class_="discount_final_price", limit=25):
-        for sells in soup.find_all("a", class_="search_result_row ds_collapse_flag", limit=25):
-            texttext = sellss.get_text()
-            spisok = f'{texttext}, [ссылка]({sells.get("href")})'
-            listlinks.append(spisok)
+    for sells in soup.find_all("div",class_="discount_final_price", limit=25):
+        listprices.append(sells.get_text())
+     
+    for sells in soup.find_all("a", class_="search_result_row ds_collapse_flag", limit=25):
+        listlinks.append(sells.get("href"))
 
-    
+    a = 0
+    while a < len(listprices):
+        spisok = f'{listprices[a]}, [ссылка]({listlinks[a]})'
+        listoflists.append(spisok)
+        a=a+1
+        print(a)
     
     #Заполнение словаря.
-    ddict = dict(zip(list, listlinks))
+    ddict = dict(zip(list, listoflists))
     
     
 
     #Вот такой вывод получился, надо будет импортировать переменную в файл с ботом (from main import result вроде).
     result = json.dumps(ddict, indent=4, ensure_ascii=False).replace('"', '').replace(',', '').replace('{','').replace('}','')
+    print(result)
     
     
         
